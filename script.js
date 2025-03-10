dfe707c2f3534a313c6362d1fdb4eb75375c3454
Pignataro67 <Pignataro67@gmail.com>
2025-03-10 14:56:41 -0700

    CSS

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ window.addEventListener('keydown', (event) => {
 <div class="key">
   ${event.keyCode}
   <small>event.keyCode</small>
+</div>
+
+<div class="key">
+  ${event.code}
+  <small>event.code</small>
 
   `
 })
